8dd3cfddd05012c74fef06dbf8c21e7ab91e3a07
Iaannn06 <alfianazad05@gmail.com>
2024-04-26 10:12:31 +0700

    membuat tampilan untuk case 2

--- 126_singlelinkedlist/126_singlelinkedlist.cpp
+++ 126_singlelinkedlist/126_singlelinkedlist.cpp
@@ -155,6 +155,18 @@ int main() {
 					break;
 				}
 
+				int nim;
+				cout << "Masukkan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << nim << " berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
+
 		
 			
 	
