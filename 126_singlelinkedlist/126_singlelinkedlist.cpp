9e7a13e328c96d27016e2ed286bc1e76df4e5801
Iaannn06 <alfianazad05@gmail.com>
2024-04-26 10:05:53 +0700

    membuat if else untuk search node

--- 126_singlelinkedlist/126_singlelinkedlist.cpp
+++ 126_singlelinkedlist/126_singlelinkedlist.cpp
@@ -31,4 +31,43 @@ void addNode() {
 		START = nodeBaru;
 		return;
 	}
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+}
+
+bool serachNode(int nim, Node* current, Node* previous) {
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
